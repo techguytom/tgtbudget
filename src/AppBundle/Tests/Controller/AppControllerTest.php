baf150db23761fc905d4c11e7ffa953922818a20
Tom Jenkins <tom@thejenkinsweb.com>
2014-12-16 21:49:57 -0600

    Security cleanup, Bill addition

--- src/AppBundle/Tests/Controller/AppControllerTest.php
+++ src/AppBundle/Tests/Controller/AppControllerTest.php
@@ -4,7 +4,7 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class AppControllerTest extends WebTestCase
 {
     public function testIndex()
     {
