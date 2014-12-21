0264d215866fbfc12c04f2fbd84b1e889f792b81
Tom Jenkins <tom@thejenkinsweb.com>
2014-12-21 11:47:43 -0600

    registered menu service, fixed bootstrap display errors

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -5,13 +5,30 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Class AppController
+ *
+ * @package AppBundle\Controller
+ */
 class AppController extends Controller
 {
     /**
+     * Home Page
+     *
      * @Route("/", name="homepage")
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('AppBundle::index.html.twig');
+    }
+
+    /**
+     * Account Page
+     *
+     * @Route("/account", name="account")
+     */
+    public function AccountAction()
+    {
+        return $this->render('AppBundle:Account:index.html.twig');
     }
 }
--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -3,27 +3,43 @@
 namespace AppBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
-use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class Builder
+ *
  * @package AppBundle\Menu
  */
-class Builder extends ContainerAware
+class Builder
 {
+    /**
+     * @var FactoryInterface
+     */
+    private $factory;
+
+    /**
+     * Constructor
+     *
+     * @param $factory FactoryInterface
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
     /**
      * Main Menu
      *
-     * @param FactoryInterface $factory
-     * @param array $options
+     * @param $request Request
      *
      * @return \Knp\Menu\ItemInterface
      */
-    public function mainMenu(FactoryInterface $factory, array $options)
+    public function createMainMenu(Request $request)
     {
-        $menu = $factory->createItem('root');
+        $menu = $this->factory->createItem('root');
 
         $menu->addChild('Home', array('route' => 'homepage'));
+        $menu->addChild('Account', array('route' => 'account'));
 
         return $menu;
     }
